fe2012eb8d1206aa5b260a5281e3e2d1651a0cac
sanskarjhala <sanskarjhala@gmail.com>
2025-06-29 19:02:02 +0530

    review controllers added

--- server/src/controllers/Review.ts
+++ server/src/controllers/Review.ts
@@ -1,10 +1,88 @@
 import { Request, Response } from "express";
+import prisma from "../utils/prisma";
+import { JwtPayload } from "jsonwebtoken";
 
-export const getReviews = async(req:Request , res:Response) => {
-    
-}
+export const getReviews = async (req: Request, res: Response) => {
+  const { bookId } = req.query;
 
-//protected
-export const postReview = async(req:Request , res:Response) => {
+  if (!bookId) {
+    res.status(400).json({ error: " bookd-id is required" });
+    return;
+  }
 
-} 
+  const reviews = await prisma.review.findMany({
+    where: { bookId: bookId as string },
+    include: { user: true },
+    orderBy: { created_at: "desc" },
+  });
+
+  res.json(
+    reviews.map((review) => ({
+      id: review.id,
+      bookId: review.bookId,
+      userId: review.userId,
+      userName: review.user.username,
+      rating: review.rating,
+      comment: review.comment,
+      createdAt: review.created_at,
+    }))
+  );
+  return;
+};
+
+//protected user
+export const createReview = async (req: Request, res: Response) => {
+  const user = req.user as JwtPayload;
+  const { bookId, rating, comment } = req.body;
+
+  if (!bookId || !comment || !rating) {
+    return res.status(400).json({ error: "Missing fields" });
+  }
+
+  if (comment.length < 10 || comment.length > 1000)
+    return res
+      .status(400)
+      .json({ error: "Comment must be 10-1000 characters" });
+
+  const existing = await prisma.review.findUnique({
+    where: {
+      userId_bookId: {
+        userId: user.id,
+        bookId: bookId,
+      },
+    },
+  });
+
+  if (existing)
+    return res
+      .status(409)
+      .json({ error: "You have already reviewed this book" });
+
+  const review = await prisma.review.create({
+    data: {
+      userId: user.id,
+      bookId: bookId,
+      rating,
+      comment,
+    },
+  });
+
+  const [avg, count] = await prisma.$transaction([
+    prisma.review.aggregate({
+      where: { bookId: bookId },
+      _avg: { rating: true },
+    }),
+    prisma.review.count({ where: { bookId: bookId } }),
+  ]);
+
+  await prisma.book.update({
+    where: { id: bookId },
+    data: {
+      rating: avg._avg.rating ?? 0,
+      review_count: count,
+    },
+  });
+
+    res.status(201).json({ ...review, userName: user.username });
+    return;XMLDocument
+};
