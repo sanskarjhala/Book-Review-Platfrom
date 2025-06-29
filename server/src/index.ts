d03426d55c3a5028d2e11f63791c332e1993d0a7
sanskarjhala <sanskarjhala@gmail.com>
2025-06-29 21:33:50 +0530

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,18 @@
-import  express  from "express";
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import authRoutes from "./routes/auth.routes"
-// import bookRoutes from "./routes/book.routes"
+import authRoutes from "./routes/auth.routes";
+import bookRoutes from "./routes/book.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
-app.use(express.json())
-app.use("/auth" , authRoutes)
-// app.use("/book" , bookRoutes)
-
-
+app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/", bookRoutes);
+app.use("/users", userRoutes);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port , () => {
-    console.log(`server is running at port number ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running at port number ${port}`);
+});
