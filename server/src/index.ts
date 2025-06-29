c9a8c6649c225119d76309f2b8339daf621ea190
sanskarjhala <sanskarjhala@gmail.com>
2025-06-29 22:14:47 +0530

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,10 @@ dotenv.config();
 import authRoutes from "./routes/auth.routes";
 import bookRoutes from "./routes/book.routes";
 import userRoutes from "./routes/user.routes";
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/", bookRoutes);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001'; // Update this to your backend URL
+const API_BASE_URL = "http://localhost:8081"; // Update this to your backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
