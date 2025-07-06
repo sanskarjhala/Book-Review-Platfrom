5f3f57075ff867c2d1dc0f0b6f891688e0ac3216
sanskarjhala <sanskarjhala@gmail.com>
2025-07-06 11:01:31 +0530

    fixing files

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ app.use("/auth", authRoutes);
 app.use("/", bookRoutes);
 app.use("/", userRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 app.listen(port, () => {
   console.log(`server is running at port number ${port}`);
